7f64fa46bc75fb75ecac96bfff3a648b62030c86
Lukeem <eluqman012@gmail.com>
2024-11-10 10:15:01 +0700

    message

---
+++ src/Sesi_7/GajiLembur.java
@@ -0,0 +1,22 @@
+package Sesi_7;
+
+import java.util.Scanner;
+
+public class GajiLembur {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int gajiBulanan, jamLembur, totalLembur, upahPerJam;
+        System.out.print("Masukkan gaji bulanan : ");
+        gajiBulanan = sc.nextInt();
+        System.out.print("Masukkan jam lembur : ");
+        jamLembur = sc.nextInt();
+        upahPerJam = gajiBulanan/173;
+        if (jamLembur >= 4){
+            totalLembur = gajiBulanan + (4*upahPerJam) + (4*2*upahPerJam);
+            System.out.println("Total gaji  lembur : " + totalLembur);
+        }else{
+            totalLembur = gajiBulanan + (jamLembur*upahPerJam);
+            System.out.println("Total gaji  lembur : " + totalLembur);
+        }sc.close();
+    }
+}
---
+++ src/Sesi_7/Looping.java
@@ -0,0 +1,16 @@
+package Sesi_7;
+
+public class Looping {
+    public static void main(String[] args) {
+        for(int x=1;x <= 4; x++ ){
+            for(int y=1;y <= 4; y++){
+                if(y <= 4 - x){
+                    System.out.print("S ");
+                }else{
+                    System.out.print("O ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
